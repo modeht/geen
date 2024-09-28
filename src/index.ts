bf1b8de9f20372cbcf48fde6bbb8e0a40683c7ff
modeht <modeht@gmail.com>
2024-09-28 22:45:03 +0300

    promising

--- src/index.ts
+++ src/index.ts
@@ -11,63 +11,67 @@ function parseTsFile(file: string): ts.SourceFile {
 	return sourceFile;
 }
 
-const file = readTsFile(join(process.cwd(), 'entities', 'category.entity.ts'));
-const ast = parseTsFile(file);
-
-const extractDecorators = (ds: readonly ts.Decorator[] | undefined) => {
-	if (!ds) return;
-
-	function getExpres(dlvl = 1, node: ts.Node) {
-		if (dlvl > 1) return;
-		console.log('node text', node.getText());
-		console.log('kind', getKindLabel(node.kind));
-		console.log('-------------------------');
-		dlvl++;
-		node.forEachChild((...args) => getExpres(dlvl, ...args));
-	}
-
-	for (const d of ds) {
-		getExpres(1, d);
-	}
-};
-const r: Record<string, any> = {};
-const visit = (node: ts.Node): void => {
-	if (ts.isClassDeclaration(node)) {
-		// if (node.name?.text) classes[node?.name?.text] = node;
-		console.log('class', node.getText());
-	}
-	// if (ts.isDecorator(node)) {
-	// 	console.log('decorator', node?.getText());
-	// }
-	if (ts.isPropertyDeclaration(node)) {
-		extractDecorators(ts.getDecorators(node));
-
-		// console.log(node.name?.getText());
-		// console.log(node.type?.getText());
-		// console.log('property', node?.getText());
-	}
-	// if(ts.i)
-
-	node.forEachChild(visit);
-};
-
-visit(ast);
-
-// console.log(classes);
-
-// const findFields = (nodes: ts.Node[]): void => {
-// 	for (const node of nodes) {
-// 		// node.forEachChild((child) => {
-// 		// 	const { parent, ...rest } = child;
-// 		// 	console.log(getKindLabel(child.kind));
-// 		// 	console.log(child.getText());
-// 		// 	console.log(rest);
-// 		// });
-// 	}
-// };
-
-// findFields(Object.values(classes));
-
 function getKindLabel(n: number) {
 	return Object.entries(ts.SyntaxKind).find(([k, v]) => v === n);
 }
+
+const file = readTsFile(join(process.cwd(), 'entities', 'category.entity.ts'));
+const ast = parseTsFile(file);
+
+// const extractDecorators = (ds: readonly ts.Decorator[] | undefined) => {
+// 	if (!ds) return;
+
+// 	function getExpres(dlvl = 1, node: ts.Node) {
+// 		if (dlvl > 1) return;
+// 		console.log('node text', node.getText());
+// 		console.log('kind', getKindLabel(node.kind));
+// 		console.log('-------------------------');
+// 		dlvl++;
+// 		node.forEachChild((...args) => getExpres(dlvl, ...args));
+// 	}
+
+// 	for (const d of ds) {
+// 		getExpres(1, d);
+// 	}
+// };
+
+const r: Record<string, any> = {};
+const visit = (prev: any, node: ts.Node): void => {
+	let curr = prev;
+	// console.log(prev);
+	if (ts.isImportDeclaration(node)) {
+		curr = {
+			module: node.moduleSpecifier?.getText(),
+		};
+		prev['imports'] = [...(prev['imports'] || []), curr];
+	} else if (ts.isClassDeclaration(node)) {
+		curr = {
+			name: node?.name?.getText(),
+		};
+		prev['classes'] = [...(prev['classes'] || []), curr];
+	} else if (ts.isPropertyDeclaration(node)) {
+		curr = {
+			name: node?.name?.getText(),
+			type: node?.type?.getText(),
+		};
+		prev['properties'] = [...(prev['properties'] || []), curr];
+	} else if (ts.isDecorator(node)) {
+		curr = {
+			fullText: node?.getText(),
+		};
+		prev['decorators'] = [...(prev['decorators'] || []), curr];
+	} else if (ts.isCallExpression(node)) {
+		console.log(node?._declarationBrand);
+		console.log(node?._expressionBrand);
+		console.log(node?._leftHandSideExpressionBrand);
+		console.log(node?.expression?.getText());
+	}
+	node.forEachChild((child) => visit(curr, child));
+};
+
+visit(r, ast);
+
+console.dir(r, { depth: null });
+
+//get all classes
+//find that entity?
