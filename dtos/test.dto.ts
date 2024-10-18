4b7fdc26eba9d606fdc706ca83e5f56bf5c9d097
modeht <modeht@gmail.com>
2024-10-18 22:46:21 +0300

    dtos updates

---
+++ dtos/test.dto.ts
@@ -0,0 +1,41 @@
+import { CategoryFilterEntity } from '../../category-fitlers/entities/category-filters.entity';
+import { TranslationEntity } from '../../translations/entities/translation.entity';
+import { MediaEntity } from '../../media/entities/media.entity';
+import { AdEntity } from '../../ads/entities/ad.entity';
+import { UserEntity } from '../../users/entities/user.entity';
+import { UserInterestEntity } from '../../users/entities/user-interests.entity';
+import { BannerEntity } from '../../banners/entities/banner.entity';
+import * as v from "class-validator";
+import * as t from "class-transformer";
+export class AddCategoryEntityDto {
+@v.IsString()
+@v.IsOptional()
+name?: string | null;
+@v.IsOptional()
+interestedIn?: UserInterestEntity[] | null;
+@v.IsOptional()
+banners?: BannerEntity | null;
+@v.IsOptional()
+icon?: MediaEntity | null;
+@v.IsOptional()
+translations?: TranslationEntity[] | null;
+@v.IsOptional()
+filters?: CategoryFilterEntity[] | null;
+
+ads: AdEntity[];
+@v.IsOptional()
+filter?: CategoryFilterEntity | null;
+@v.IsBoolean()
+visible: boolean;
+@v.IsBoolean()
+isArchived: boolean;
+@v.IsNumber()
+@v.IsOptional()
+index?: number | null;
+@v.IsDate()
+@t.Type(()=>Date)
+createdAt: Date;
+@v.IsDate()
+@t.Type(()=>Date)
+updatedAt: Date;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import ts from 'typescript';
 import { readFileSync } from 'fs';
 import { join } from 'path';
+import { readFile, writeFile } from 'fs/promises';
+import { dir, log } from 'console';
 
 function readTsFile(path: string) {
 	return readFileSync(path, 'utf8');
@@ -18,62 +20,144 @@ function getKindLabel(n: number) {
 const file = readTsFile(join(process.cwd(), 'entities', 'category.entity.ts'));
 const ast = parseTsFile(file);
 
-const r: Record<string, any> = {};
-const visit = (prev: any, node: ts.Node): void => {
-	let curr = prev;
-	if (ts.isImportDeclaration(node)) {
-		curr = {
-			module: node?.moduleSpecifier?.getText(),
-		};
-		prev['imports'] = [...(prev['imports'] || []), curr];
-	} else if (ts.isClassDeclaration(node)) {
-		curr = {
-			name: node?.name?.getText(),
-		};
-		prev['classes'] = [...(prev['classes'] || []), curr];
-	} else if (ts.isPropertyDeclaration(node)) {
-		curr = {
-			name: node?.name?.getText(),
-			type: node?.type?.getText(),
-		};
-		prev['properties'] = [...(prev['properties'] || []), curr];
-	} else if (ts.isDecorator(node)) {
-		curr = {
-			fullText: node?.getText(),
-		};
-		// if (node?.getText() === "@Entity({\r\n\tname: 'categories',\r\n})") {
-		// 	const { parent, ...rest } = node;
-		// 	console.dir(rest, { depth: 2 });
-		// }
-		prev['decorators'] = [...(prev['decorators'] || []), curr];
-	} else if (ts.isIdentifier(node)) {
-		curr = {
-			expression: node?.getText(),
-		};
-		prev['identifiers'] = [...(prev['identifiers'] || []), curr];
-	} else if (ts.isCallExpression(node)) {
-		curr = {
-			expression: node?.expression?.getText(),
-		};
-		prev['functions'] = [...(prev['functions'] || []), curr];
-	} else if (ts.isArrowFunction(node)) {
-		curr = {
-			def: node?.getText(),
-		};
-		prev['arrowFn'] = [...(prev['arrowFn'] || []), curr];
-	} else if (ts.isPropertyAssignment(node)) {
-		curr = {
-			statement: node?.getText(),
-		};
-		prev['props'] = [...(prev['props'] || []), curr];
+function parseTree(ast) {
+	let start = {};
+	const visit = (prev, node: ts.Node) => {
+		let curr = prev;
+		if (ts.isImportDeclaration(node)) {
+			curr = {
+				module: node?.moduleSpecifier?.getText(),
+				fullText: node?.getFullText(),
+			};
+			prev['imports'] = [...(prev['imports'] || []), curr];
+		} else if (ts.isClassDeclaration(node)) {
+			curr = {
+				name: node?.name?.getText(),
+			};
+			prev['classes'] = [...(prev['classes'] || []), curr];
+		} else if (ts.isPropertyDeclaration(node)) {
+			curr = {
+				name: node?.name?.getText(),
+				type: node?.type?.getText(),
+			};
+			prev['properties'] = [...(prev['properties'] || []), curr];
+		} else if (ts.isDecorator(node)) {
+			curr = {
+				fullText: node?.getText(),
+			};
+			prev['decorators'] = [...(prev['decorators'] || []), curr];
+		} else if (ts.isIdentifier(node)) {
+			curr = {
+				expression: node?.getText(),
+			};
+			prev['identifiers'] = [...(prev['identifiers'] || []), curr];
+		} else if (ts.isCallExpression(node)) {
+			curr = {
+				expression: node?.expression?.getText(),
+			};
+			prev['functions'] = [...(prev['functions'] || []), curr];
+		} else if (ts.isArrowFunction(node)) {
+			curr = {
+				def: node?.getText(),
+			};
+			prev['arrowFn'] = [...(prev['arrowFn'] || []), curr];
+		} else if (ts.isPropertyAssignment(node)) {
+			curr = {
+				statement: node?.getText(),
+			};
+			prev['props'] = [...(prev['props'] || []), curr];
+		}
+		node.forEachChild((child) => visit(curr, child));
+	};
+	visit(start, ast);
+	return start;
+}
+const fileRep = parseTree(ast);
+dir(fileRep, { depth: null });
+
+async function createAddDto(file: Record<string, any>, depth = 3, iterator = 1) {
+	const entityClass = file.classes?.find((c) =>
+		c.decorators.find((d) => d.fullText.startsWith('@Entity'))
+	);
+	if (!entityClass) return;
+
+	const entityName = entityClass.name;
+	const dtoClassName = `Add${entityName}Dto`;
+
+	//handle imports
+	const imports: string[] = file.imports
+		?.filter((i) => i.module.startsWith("'."))
+		.map((i) => i.fullText.trim().replace('\n', ''));
+	imports.push('import * as v from "class-validator";');
+	imports.push('import * as t from "class-transformer";');
+
+	//handle properties fields
+	const fields = createFieldsWithValidations(entityClass.properties);
+
+	try {
+		let dtoTemplate = await readFile(
+			join(process.cwd(), 'templates/dto.template.txt'),
+			'utf8'
+		);
+		dtoTemplate = dtoTemplate.replace('<<dtoClass>>', dtoClassName);
+		dtoTemplate = dtoTemplate.replace('<<properties>>', fields.join('\n'));
+		dtoTemplate = dtoTemplate.replace('<<imports>>', imports.join('\n'));
+		await writeFile(join(process.cwd(), 'dtos/test.dto.ts'), dtoTemplate);
+	} catch (error) {
+		console.error(error);
 	}
-	node.forEachChild((child) => visit(curr, child));
+}
+
+// createAddDto(fileRep);
+
+type Field = {
+	name: string;
+	type: string;
+	decorators: Decorator[];
 };
 
-visit(r, ast);
+type Decorator = {
+	fullText: string;
+	functions: any[];
+};
 
-console.dir(r, { depth: null });
+function createFieldsWithValidations(fields: Field[], { includeRelations = true }) {
+	const fieldsStringified: string[] = [];
+	for (const field of fields) {
+		if (field.name === 'id') continue;
+		let fieldText = '';
+		let fieldNullable = false;
+		const validations: string[] = [];
+		const types = field.type?.split('|').map((t) => t.trim());
+		for (const type of types) {
+			if (type === 'null' || type === 'undefined') {
+				validations.push('@v.IsOptional()');
+				fieldNullable = true;
+			} else if (type === 'string') {
+				validations.push('@v.IsString()');
+			} else if (type === 'string[]') {
+				validations.push('@v.IsString({each:true})');
+			} else if (type === 'number') {
+				validations.push('@v.IsNumber()');
+			} else if (type === 'number[]') {
+				validations.push('@v.IsNumber({},{each:true})');
+			} else if (type === 'Date') {
+				validations.push('@v.IsDate()\n@t.Type(()=>Date)');
+			} else if (type === 'boolean') {
+				validations.push('@v.IsBoolean()');
+			}
+			if (!includeRelations) continue;
+			//handle enums
+			//handle relationships
+		}
 
-//get all classes
-//find that entity?
-// console.log(getKindLabel(80));
+		const decorators = field.decorators?.map((d) => d.fullText);
+		for (const deco of decorators) {
+		}
+
+		fieldText += validations.join('\n') + '\n';
+		fieldText += `${field.name}${fieldNullable ? '?' : ''}: ${field.type};`;
+		fieldsStringified.push(fieldText);
+	}
+	return fieldsStringified;
+}
