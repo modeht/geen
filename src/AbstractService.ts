a4f72a4c5c0d0124d1b53e0d38e1f2bd900566f8
modeht <modeht@gmail.com>
2024-10-26 11:49:07 +0300

    abstract service

---
+++ src/AbstractService.ts
@@ -0,0 +1,23 @@
+
+	async generatedCreate(body: AddCategoryEntityDto) {
+		const bodyInstance = plainToInstance(AddCategoryEntityDto, body);
+		const row = new CategoryEntity();
+		for (const key in bodyInstance) {
+			let val = bodyInstance[key];
+			if (!val) continue;
+			const relation = Reflect.getMetadata('relation', bodyInstance, key);
+			if (relation) {
+				if (relation.type === 'hasOne') {
+					const childRel = await this.datasource.manager.save(relation.entity, val);
+					val = childRel;
+				}
+				if (relation.type === 'belongsToOne') {
+					const childRel = await this.datasource.manager.save(relation.entity, val);
+					val = childRel;
+				}
+			}
+			row[key] = val;
+		}
+		const r = await this.datasource.manager.save('CategoryEntity', row);
+		return r;
+	}
