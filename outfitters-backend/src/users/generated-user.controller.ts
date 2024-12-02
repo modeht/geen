fba06932aa5e3807d29b18e74f7dfe7aa2101e1c
modeht <modeht@gmail.com>
2024-12-02 17:21:21 +0200

    refactors

--- outfitters-backend/src/users/generated-user.controller.ts
+++ outfitters-backend/src/users/generated-user.controller.ts
@@ -1,57 +1,56 @@
 import { Controller, Post, Get, Put, Param } from '@nestjs/common';
-import CreateUserSchema, {
-	TCreateUserSchemaInput,
-	TCreateUserSchemaOutput,
-} from './generated-schemas//create-user.schema';
-import UpdateUserSchema, {
-	TUpdateUserSchemaInput,
-	TUpdateUserSchemaOutput,
-} from './generated-schemas//update-user.schema';
-import ReadUserSchema, {
-	TReadUserSchemaInput,
-	TReadUserSchemaOutput,
-} from './generated-schemas//read-user-query.schema';
-import { UserEntity } from './entities/user.entity';
-import { ApiBody, ApiQuery } from '@nestjs/swagger';
-import { SchemaDefs } from '../schema-defs';
-import { MoBody } from '../globals/decorators/mo-body.decorator';
-import { MoQuery } from '../globals/decorators/mo-query.decorator';
-import { UserService } from './generated-user.service';
+import CreateUserSchema, { TCreateUserSchemaInput, TCreateUserSchemaOutput } from './generated-schemas//create-user.schema'
+import UpdateUserSchema, { TUpdateUserSchemaInput, TUpdateUserSchemaOutput } from './generated-schemas//update-user.schema'
+import ReadUserSchema, { TReadUserSchemaInput, TReadUserSchemaOutput } from './generated-schemas//read-user-query.schema'
+import { UserEntity } from './entities/user.entity'
+import { ApiBody, ApiQuery } from '@nestjs/swagger'
+import { SchemaDefs } from "../schema-defs"
+import { MoBody } from "../globals/decorators/mo-body.decorator"
+import { MoQuery } from "../globals/decorators/mo-query.decorator"
+import { UserService } from './generated-user.service'
 
 @Controller('user')
 export class UserController {
-	constructor(private service: UserService) {}
+  
+  constructor(private service: UserService){}
+  
+			@Post()
+			@ApiBody({
+				schema:{
+					$ref: SchemaDefs.CreateUser
+				}
+			})
+			async create(
+				@MoBody(CreateUserSchema) body: TCreateUserSchemaOutput,
+			) {
+				return this.service.createRow(body);
+			}
+		
 
-	@Post()
-	@ApiBody({
-		schema: {
-			$ref: SchemaDefs.CreateUser,
-		},
-	})
-	async create(@MoBody(CreateUserSchema) body: TCreateUserSchemaOutput) {
-		return this.service.createRow(body);
-	}
+			@Put(':id')
+			@ApiBody({
+				schema:{
+					$ref: SchemaDefs.UpdateUser
+				}
+			})
+			async update(
+				@Param('id') id: string,
+				@MoBody(UpdateUserSchema) body: TUpdateUserSchemaOutput,
+			) {
+				return this.service.updateRow(+id, body);
+			}
+		
 
-	@Put(':id')
-	@ApiBody({
-		schema: {
-			$ref: SchemaDefs.UpdateUser,
-		},
-	})
-	async update(
-		@Param('id') id: string,
-		@MoBody(UpdateUserSchema) body: TUpdateUserSchemaOutput,
-	) {
-		return this.service.updateRow(+id, body);
-	}
-
-	@Get()
-	@ApiQuery({
-		schema: {
-			$ref: SchemaDefs.ReadUserQuery,
-		},
-	})
-	async read(@MoQuery(ReadUserSchema) query: TReadUserSchemaOutput) {
-		return this.service.readRows(query);
-	}
+			@Get()
+			@ApiQuery({
+				schema:{
+					$ref: SchemaDefs.ReadUserQuery
+				}
+			})
+			async read(
+				@MoQuery(ReadUserSchema) query: TReadUserSchemaOutput,
+			) {
+				return this.service.readRows(query);
+			}
+		
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,12 @@
 import { async, sync } from 'fast-glob';
-import { CreateDtoCreator } from './DtoCreator';
 import { parseFiles } from './file-parser';
-import { log, time, timeEnd } from 'console';
+import { time, timeEnd } from 'console';
 import { prereq } from './prereq';
-import { TreeParser } from './TreeParser';
-import { parseTreeV2 } from './tree-parser';
-import { CreateSchemaCreator } from './CreateSchemaCreator';
-import { ReadSchemaFiltersCreator } from './ReadSchemaFiltersCreator';
-import { ReadSchemaRelationsCreator } from './ReadSchemaRelationsCreator';
-import { ReadSchemaCreator } from './ReadSchemaCreator';
-import { UpdateSchemaCreator } from './UpdateSchemaCreator';
 import { ModuleCreator } from './ModuleCreator';
 
 async function main() {
 	time('Loading entities');
-	const allEntities = await async('**/*/*.entity.ts', { absolute: true, ignore: ['**/*/node_modules'] });
+	const allEntities = await async('**/*/*.entity.ts', { absolute: true, onlyFiles: true, ignore: ['**/node_modules'] });
 	timeEnd('Loading entities');
 
 	time('Parsing');
@@ -25,31 +17,12 @@ async function main() {
 	await prereq();
 	timeEnd('Prerequistes');
 
-	time('Creating dtos');
-	// console.dir(TreeParser.parse(ASTs[Object.keys(ASTs)[0]].sourceFile), { depth: null });
-
-	// const addDtoCreator = new ReadSchemaCreator(
-	// 	ASTs['category.entity'].sourceFile,
-	// 	ASTs['category.entity'].fullPath,
-	// 	ASTs,
-	// 	{
-	// 		maxDepth: 1,
-	// 		currDepth: 0,
-	// 	}
-	// );
-	// addDtoCreator.baseSetup();
-	// await addDtoCreator.buildFile();
-
-	// const acc = {};
-	// parseTreeV2(acc, ASTs[Object.keys(ASTs)[0]].sourceFile);
-	// console.dir(acc, { depth: null });
-	// console.log(Object.keys(ASTs).length);
+	time('Creating modules');
 	for (const ast in ASTs) {
 		const m = new ModuleCreator(ASTs[ast].fullPath, ASTs[ast].sourceFile, ASTs);
-		m.build()
+		m.build();
 	}
-
-	timeEnd('Creating dtos');
+	timeEnd('Creating modules');
 }
 
 main();
